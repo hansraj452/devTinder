cf6204cba3aeaec8f2a7367d34a51209ef113b4e
hansraj452 <hrs638698@gmail.com>
2026-04-18 18:05:03 +0530

    Now mutiple routes can be created

--- src/app.js
+++ src/app.js
@@ -4,23 +4,21 @@ const express = require('express')
 
 const app = express();
 
-//this will match all the the https methods api calls to /123
-app.use('/123' , (req , res) =>{
-    res.send("New route have been created for the /123")
-})
 
+// app.use() can handle all kind of routes like post get delete and updadte and patch.
 
-//app.get will only look get api call methods and send their response 
-app.get("/user" , (req , res)=>{
-    res.send({firstName : "shyam" , last :"sharma"})
-})
+// In single router multile routes can be send but if we not return form the first route then it will hang into the first route and send error in the response
+app.use('/user' , (req , res) =>{
+    console.log("handle route one ")
+    // if we rmove the  res.send from the first router response then it will hang in the 
+    // res.send("response 1 is send")
+},
+(req , res) =>{
+console.log("handle router  two")
+res.send("response 2 is send")
+}
 
-app.post("/user" , (req , res) =>{
-    console.log("Data wil save to db")
-    res.send({message:"Data saved successfully into the db"})
-})
-
-
+)
 
 app.listen('8000' , () =>{
     console.log(`Server is on running port ${8000}`)
