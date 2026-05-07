414a04c04a383df4a4aabcfe81cff2bef0691502
hansraj452 <hrs638698@gmail.com>
2026-05-08 00:39:55 +0530

    Creating the api of send connection request with all the validiona and index

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { schema } = require('./user');
+const {Schema} = mongoose;
+
+const connectionRequestSchema =  new Schema(
+    {
+    fromUserId :{
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
+    },
+    toUserId : {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status : {
+        type : String ,
+        enum :{
+            values: ["ignored" , "interested" , "accepted" , "rejected"],
+            message : `{VALUE} is not supported`
+        },
+        required : true
+    }
+    
+},
+{
+    timestamps : true,
+}
+
+)
+
+// pre is a kind of middle ware 
+connectionRequestSchema.pre('save', function(next) {
+    const connectionRequest = this;
+
+    if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("Connection request cannot be sent to yourself!");
+    }
+
+    
+    next();
+});
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+const ConnectionRequest = new mongoose.model('ConnectionRequest' , connectionRequestSchema);
+module.exports = ConnectionRequest
