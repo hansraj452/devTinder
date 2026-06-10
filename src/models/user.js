721e7f72e29ac416eccd1f4cce4d8604012c60ce
hansraj452 <hrs638698@gmail.com>
2026-06-11 00:06:27 +0530

    Add send email configration and env file

--- src/models/user.js
+++ src/models/user.js
@@ -78,7 +78,7 @@ const userSchema = new Schema(
 
 userSchema.methods.getJWT = async function () {
   const user = this
-  const token = await jwt.sign({ _id: user._id }, "DEV@TINDER$9970", {
+  const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
           expiresIn: "7d",
         });
    return token;
