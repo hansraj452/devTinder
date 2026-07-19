9cc1797e943fb3f8a46a4b0724868bb7092c2cdd
hansraj452 <hrs638698@gmail.com>
2026-07-19 16:02:54 +0530

    Bug fix of chat

--- src/utility/socketIo.js
+++ src/utility/socketIo.js
@@ -15,38 +15,44 @@ const initializeSocket = (server) => {
     });
 
     socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
-      //save message in the data base
-      try{
-        const roomId = [userId, targetUserId].sort().join("_");
+  try {
+    const roomId = [userId, targetUserId].sort().join("_");
 
-        let chat = await Chat.findOne({
-          participants : {$all : [userId , targetUserId]}
-
-        })
-        if(!chat){
-          chat =  new Chat({
-            participants :[userId , targetUserId],
-            messages : []
-          })
-        }
-        chat.messages.push({
-          senderId : userId,
-          text,
-        })
-
-        await chat.save();
-
-        io.to(roomId).emit("msgReceived", {
-        firstName,
-        text,
-        userId,
-      });
-
-      }
-      catch(err){
-         console.log(err)
-      }
+    let chat = await Chat.findOne({
+      participants: { $all: [userId, targetUserId] }
     });
+
+    if (!chat) {
+      chat = new Chat({
+        participants: [userId, targetUserId],
+        messages: []
+      });
+    }
+
+    // Push message and grab the new length
+    const newMsgIndex = chat.messages.push({
+      senderId: userId,
+      text,
+    }) - 1;
+
+    await chat.save();
+    
+    // Grab the actual saved message document containing the generated _id and createdAt
+    const savedMessage = chat.messages[newMsgIndex];
+
+    // Emit the complete, formatted object matching your fetchChat structure
+    io.to(roomId).emit("msgReceived", {
+      _id: savedMessage._id,
+      userId: userId,
+      firstName: firstName,
+      text: text,
+      createdAt: savedMessage.createdAt || new Date(),
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+});
 
     socket.on("disconnect", () => {
       console.log("User disconnected");
